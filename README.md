4f5efe1197733201244aa8c27271392362026805
Saket <saketk1.sk@gmail.com>
2017-06-20 13:15:12 +0530

    Removed unnecessary files. Changed other files to template form

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # PyBossa-Plugin-Template
 Template for adding plugins to PyBossa
+
+Plugins are an integral part of the PyBossa environment. They help you add new features to the existing PyBossa implementation without having to modify the core code. Plugins help improve the Crowdsourcing application to suit your specific requirements along with using PyBossa as a base.
+
+It is very easy to add plugins to your PyBossa application. Just include the __init__.py and info.json as in this template to make your plugin running within the installed PyBossa instance.
+
+Plugins can also include other flask files like views, models and forms.
+
+Simply add the required views, models and forms like in any flask application with minor changes in some files as specified in this template.
+
+To add your models to the PyBossa database, run the following python script
+
+python cli.py create_db
+
+
--- __init__.py
+++ __init__.py
@@ -1,24 +1,29 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#Import the Plugin instance
 from flask.ext.plugins import Plugin
+#Import app which is initialized in pybossa.core
 from flask import current_app as app
 
-__plugin__ = "Quiz"
+#This line is mandatory. 
+#You can give any name to your template. 
+#Recomended to give the same name as the name of the directory for your plugin.
+__plugin__ = "TemplatePlugin"
+
+#Version number for your template.
+#This is optional
 __version__ = "0.0.1"
 
-#app = Flask(__name__)
-#app.config.from_object('config')
-#db = SQLAlchemy(app)
-
-#from flask import Blueprint
-#blueprint = Blueprint('blueprint', __name__, template_folder="templates")
-
-
-
-class Quiz(Plugin):
+#Create a class with the same name as specified in line 10
+class TemplatePlugin(Plugin):
+#The setup method is called when the plugin is setup by PyBossa
     def setup(self):
+	#Import the blueprint defined in views.py
 	from .views import blueprint
-        app.register_blueprint(blueprint, url_prefix="/user_evaluation")
+	#Register the blueprint with the PyBossa app instance. 
+	#This will allow us to route to views within our plugin. 
+	#The url_prefix parameter passed while registering denotes the prefix in the URL to route 	  views within this folder. 
+	#You can give any prefix of your choice.
+        app.register_blueprint(blueprint, url_prefix="/prefix")
 
-
+#As in any flask application, we import views.py and models.py at the end to avoid circular imports
 import views, models
